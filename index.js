b57ae4bb87d20f9bb47cedd112e15ab1e0f19a7e
amarsaraswat1989 <85442667+amarsaraswat1989@users.noreply.github.com>
2021-06-07 00:01:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components"
+
+ReactDOM.render(
+  <div>
+    <App />
+    <h1>Hi</h1>
+    </div>,
+    document.getElementById("root")
+);
---
+++ note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div>
+      <h1> Javascript and React.js</h1>
+      <p> amazing bootcamp by Shaurya Sir</p>
+
+    </div>
+  );
+}
+
+export default note;
